3a7348889a76d41b4205d16760d3fb70370adb8c
xinghalo <xinghalo@163.com>
2018-08-23 16:13:14 +0800

    update class

--- data/wtbi_images.py
+++ data/wtbi_images.py
@@ -1,5 +1,6 @@
 import os
 import requests
+import cv2
 
 IMG_PATH = '/Users/xingoo/Desktop/wtbi_images/'
 
@@ -7,14 +8,19 @@ with open('photos/photos.txt','r') as f:
     lines = f.readlines()
     total = str(len(lines))
     for index, line in enumerate(lines):
-        if index > -1 :
+        if index > 371 :
             id, url = line.strip('\n').split(',')
             post_prefix = url.split('/')[-1].split('?')[0].split('.')[-1]
 
-            r = requests.request('get', url)
+            while True:
+                try:
+                    r = requests.get(url, timeout=1)
 
-            with open(IMG_PATH + id+'.'+post_prefix, 'wb') as f:
-                f.write(r.content)
-            f.close()
+                    with open(IMG_PATH + id + '.' + post_prefix, 'wb') as f:
+                        f.write(r.content)
+                    f.close()
 
-            print(str(index) + "/" + total)
+                    print(str(index) + "/" + total)
+                    break
+                except ZeroDivisionError as e:
+                    print(e.message())
---
+++ data/wtbi_merge_label.py
@@ -0,0 +1,34 @@
+import os
+import json
+
+labels = []
+
+for file in os.listdir('/Users/xingoo/PycharmProjects/keras_frcnn/data/meta/json'):
+    print(file)
+
+    if file.startswith('test_') or file.startswith('train_'):
+        type = ''
+        if file.startswith('test_pairs_'):
+            type = file.split('test_pairs_')[-1].split('.')[0]
+        if file.startswith('train_pairs_'):
+            type = file.split('train_pairs_')[-1].split('.')[0]
+
+        with open('/Users/xingoo/PycharmProjects/keras_frcnn/data/meta/json/' + file, 'r') as f:
+            content = f.read()
+            content = json.loads(content, encoding='utf-8')
+            #{"photo": 4534, "product": 7419, "bbox": {"width": 120, "top": 172, "height": 358, "left": 134}}
+            for c in content:
+                photo = c['photo']
+                bbox = c['bbox']
+                x1 = bbox['left']
+                y1 = bbox['top']
+                w = bbox['width']
+                h = bbox['height']
+                x2 = x1+w
+                y2 = y1+h
+
+                labels.append(str(photo).zfill(9)+','+str(x1)+','+str(y1)+','+str(x2)+','+str(y2)+','+str(type))
+print(len(labels))
+with open('/Users/xingoo/PycharmProjects/keras_frcnn/data/wtbi_label.txt','w') as f:
+    for label in labels:
+        f.write(label+'\n')
