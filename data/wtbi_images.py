f9a82782946090dd02e791ec977229f6fa813cb8
xinghalo <xinghalo@163.com>
2018-08-23 15:17:24 +0800

    update class

--- data/wtbi_images.py
+++ data/wtbi_images.py
@@ -7,11 +7,13 @@ with open('photos/photos.txt','r') as f:
     lines = f.readlines()
     total = str(len(lines))
     for index, line in enumerate(lines):
-        if index > 343 :
-            id, url = line.split(',')
+        if index > -1 :
+            id, url = line.strip('\n').split(',')
+            post_prefix = url.split('/')[-1].split('?')[0].split('.')[-1]
+
             r = requests.request('get', url)
 
-            with open(IMG_PATH + str(id) + '.jpg', 'wb') as f:
+            with open(IMG_PATH + id+'.'+post_prefix, 'wb') as f:
                 f.write(r.content)
             f.close()
 
