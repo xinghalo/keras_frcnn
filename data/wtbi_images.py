37afc591494a007a059e5a9df97ef21bab0c77b7
xinghalo <xinghalo@163.com>
2018-08-23 14:56:10 +0800

    update class

---
+++ data/wtbi_images.py
@@ -0,0 +1,18 @@
+import os
+import requests
+
+IMG_PATH = '/Users/xingoo/Desktop/wtbi_images/'
+
+with open('/Users/xingoo/PycharmProjects/keras_frcnn/data/photos/photos.txt','r') as f:
+    lines = f.readlines()
+    total = str(len(lines))
+    for index, line in enumerate(lines):
+        if index > 343 :
+            id, url = line.split(',')
+            r = requests.request('get', url)
+
+            with open(IMG_PATH + str(id) + '.jpg', 'wb') as f:
+                f.write(r.content)
+            f.close()
+
+            print(str(index) + "/" + total)
